2c3d48f79a49151fdfeb1224803f48c0dad9daef
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-18 21:55:26 -0500

    test

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -1,2 +1,11 @@
 println "Hello World!"
 println "Hello World 222222!"
+
+
+def command = "echo ${env}"
+def proc = command.execute()
+proc.waitFor()              
+
+println "Process exit code: ${proc.exitValue()}"
+println "Std Err: ${proc.err.text}"
+println "Std Out: ${proc.in.text}" 
