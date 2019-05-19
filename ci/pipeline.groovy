84b80ea53148d05bfd301e51112a8d2af7771a71
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-18 22:49:54 -0500

    Dokcerizando

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -1,6 +1,7 @@
 node {
     stage('Dockerizando') {
         
+        sh "ls -l"
         docker.withRegistry('https://index.docker.io/v1/', 'docker-hub') {
             def customImage = docker.build("ruby/test:${env.BUILD_ID}")
             customImage.push()
