076ab310a8229f8f64c072e6569c640763938672
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-18 22:59:18 -0500

    Dokcerizando

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -4,7 +4,8 @@ node {
         checkout scm
         
         sh "ls -l"
-        cat "script.sh"
+        sh "$(pwd)"
+        sh "cat script.sh"
         sh "docker ps -a"
         docker.withRegistry('https://index.docker.io/v1/', 'docker-hub') {
             def customImage = docker.build("ruby/test:${env.BUILD_ID}")
