e1bccde0645fef3854ef9682944fc52d8e9b77d7
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-19 01:02:30 -0500

    Dokcerizando

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -7,7 +7,9 @@ node {
         sh "ls -l ../"
         sh "ls -l ../another_test@script"
         sh "pwd"
-        //sh "cat script.sh"
+        sh "ls -l /var/jenkins_home/workspace/another_test@tmp/durable-9c83888a"
+        sh "ls -l /var/jenkins_home/workspace/another_test@tmp"
+        sh "cat /var/jenkins_home/workspace/another_test@tmp/durable-9c83888a/script.sh"
         sh "docker ps -a"
         docker.withRegistry('https://index.docker.io/v1/', 'docker-hub') {
             def customImage = docker.build("ruby/test:${env.BUILD_ID}")
