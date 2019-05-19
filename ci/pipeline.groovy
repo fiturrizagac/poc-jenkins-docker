efb02091c66182d89597a4f13ea194b15600f236
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-18 23:00:10 -0500

    Dokcerizando

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -4,7 +4,7 @@ node {
         checkout scm
         
         sh "ls -l"
-        sh "$(pwd)"
+        sh "pwd"
         sh "cat script.sh"
         sh "docker ps -a"
         docker.withRegistry('https://index.docker.io/v1/', 'docker-hub') {
