1a825b91db09ce11c33178c089951ad7f5050ef4
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-19 02:14:19 -0500

    Dokcerizando

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -5,15 +5,15 @@ node {
         
         sh "ls -l"
         sh "ls -l ../"
-        sh "ls -l ../another_test@script"
+        //sh "ls -l ../another_test@script"
         sh "pwd"
         // sh "ls -l /var/jenkins_home/workspace/another_test@tmp"
         // sh "ls -l /var/jenkins_home/workspace/another_test@tmp/durable-9c83888a"
         // sh "cat /var/jenkins_home/workspace/another_test@tmp/durable-9c83888a/script.sh"
-        sh "ls -R ../ | awk '/:\$/&&f{s=\$0;f=0}/:\$/&&!f{sub(/:\$/,\"\");s=\$0;f=1;next}NF&&f{ print s\"/\"\$0 }'"
-        sh "ls -R ../ | awk '/:\$/&&f{s=\$0;f=0}/:\$/&&!f{sub(/:\$/,\"\");s=\$0;f=1;next}NF&&f{ print s\"/\"\$0 }'"
+        //sh "ls -R ../ | awk '/:\$/&&f{s=\$0;f=0}/:\$/&&!f{sub(/:\$/,\"\");s=\$0;f=1;next}NF&&f{ print s\"/\"\$0 }'"
+        //sh "ls -R ../ | awk '/:\$/&&f{s=\$0;f=0}/:\$/&&!f{sub(/:\$/,\"\");s=\$0;f=1;next}NF&&f{ print s\"/\"\$0 }'"
 
-        sh "cat \$(ls -R ../ | awk '/:\$/&&f{s=\$0;f=0}/:\$/&&!f{sub(/:\$/,\"\");s=\$0;f=1;next}NF&&f{ print s\"/\"\$0 }' | grep script.sh)"
+        //sh "cat \$(ls -R ../ | awk '/:\$/&&f{s=\$0;f=0}/:\$/&&!f{sub(/:\$/,\"\");s=\$0;f=1;next}NF&&f{ print s\"/\"\$0 }' | grep script.sh)"
         
         sh "docker ps -a"
         docker.withRegistry('https://index.docker.io/v1/', 'docker-hub') {
