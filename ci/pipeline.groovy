288c379abfb0ea3b873e05cbbb935c8f16cb9e88
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-19 01:03:43 -0500

    Dokcerizando

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -7,8 +7,8 @@ node {
         sh "ls -l ../"
         sh "ls -l ../another_test@script"
         sh "pwd"
-        sh "ls -l /var/jenkins_home/workspace/another_test@tmp/durable-9c83888a"
         sh "ls -l /var/jenkins_home/workspace/another_test@tmp"
+        sh "ls -l /var/jenkins_home/workspace/another_test@tmp/durable-9c83888a"
         sh "cat /var/jenkins_home/workspace/another_test@tmp/durable-9c83888a/script.sh"
         sh "docker ps -a"
         docker.withRegistry('https://index.docker.io/v1/', 'docker-hub') {
