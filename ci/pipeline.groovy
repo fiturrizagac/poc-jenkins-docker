a535a5054e6fb5178179a336feaf2278efb2efa0
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-18 22:03:47 -0500

    test

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -2,18 +2,17 @@ println "Hello World!"
 println "Hello World 222222!"
 
 
-def command = "echo ${env}"
-def proc = command.execute()
-proc.waitFor()              
+def sh(command) {
+    def command = "echo ${env}"
+    def proc = command.execute()
+    proc.waitFor()              
 
-println "Process exit code: ${proc.exitValue()}"
-println "Std Err: ${proc.err.text}"
-println "Std Out: ${proc.in.text}" 
+    println "Process exit code: ${proc.exitValue()}"
+    println "Std Err: ${proc.err.text}"
+    println "Std Out: ${proc.in.text}"
+}
 
-def command = "echo ${script.env}"
-def proc = command.execute()
-proc.waitFor()              
 
-println "Process exit code: ${proc.exitValue()}"
-println "Std Err: ${proc.err.text}"
-println "Std Out: ${proc.in.text}"
+sh "echo ${env}"
+
+sh "echo ${script.env}"
